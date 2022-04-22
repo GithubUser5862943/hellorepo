86d580b67e1c7ee960269511185f0bde119c97f7
Paul <paul.carrizales@my.utsa.edu>
2022-04-22 12:07:55 -0500

    Second commit

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Github");
 		System.out.println("First change");
+		System.out.println("Second change");
 	}
 
 }
